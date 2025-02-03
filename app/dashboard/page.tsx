6bf33f712767f7a82fc440c4b9894011039a1aaa
hudsor01 <hudsor01@icloud.com>
2025-02-02 19:00:01 -0600

    Auto-save: Sun Feb  2 19:00:00 CST 2025

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,5 @@
-// app/(dashboard)/dashboard/page.tsx
-import { MaintenanceOverview } from '@/components/dashboard/maintenance-overview'
-import { OccupancyRates } from '@/components/dashboard/occupancy-rates'
 import { PropertyMetrics } from '@/components/dashboard/property-metrics'
-import { RecentActivities } from '@/components/dashboard/recent-activities'
 import { RevenueChart } from '@/components/dashboard/revenue-chart'
-import { UpcomingPayments } from '@/components/dashboard/upcoming-payments'
 import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
 import { Suspense } from 'react'
@@ -27,34 +22,6 @@ export default function DashboardPage() {
         <Grid item xs={12} lg={8}>
           <Suspense fallback={<RevenueChart.Skeleton />}>
             <RevenueChart />
-          </Suspense>
-        </Grid>
-
-        {/* Occupancy Rates - Takes up 4 columns */}
-        <Grid item xs={12} lg={4}>
-          <Suspense fallback={<OccupancyRates.Skeleton />}>
-            <OccupancyRates />
-          </Suspense>
-        </Grid>
-
-        {/* Maintenance Overview - 6 columns */}
-        <Grid item xs={12} md={6}>
-          <Suspense fallback={<MaintenanceOverview.Skeleton />}>
-            <MaintenanceOverview />
-          </Suspense>
-        </Grid>
-
-        {/* Recent Activities - 6 columns */}
-        <Grid item xs={12} md={6}>
-          <Suspense fallback={<RecentActivities.Skeleton />}>
-            <RecentActivities />
-          </Suspense>
-        </Grid>
-
-        {/* Upcoming Payments - Full width */}
-        <Grid item xs={12}>
-          <Suspense fallback={<UpcomingPayments.Skeleton />}>
-            <UpcomingPayments />
           </Suspense>
         </Grid>
       </Grid>
